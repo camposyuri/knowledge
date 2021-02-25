2b0ede27cc9045ff6c8aea7e7f2b2a365febfbea
camposyuri <yurinapoleao@gmail.com>
2021-02-25 19:39:02 -0300

    Criando a routa e metodo save() no usuario

---
+++ backend/api/user.js
@@ -0,0 +1,9 @@
+module.exports = (app) => {
+  const save = (req, res) => {
+    res.send("User save");
+  };
+
+  return {
+    save,
+  };
+};
--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,11 @@ const app = require("express")();
 const consign = require("consign");
 
 // Injetando o app que tem o EXPRESS nele dentro do meu middlewares.js
-consign().then("./config/middlewares.js").into(app);
+consign()
+  .then("./config/middlewares.js")
+  .then("./api")
+  .then("./config/routes.js")
+  .into(app);
 
 app.listen(3333, () => {
   console.log("Backend executando...");
