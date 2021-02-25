c655935c76259f9d526999b40b44d231e45dbb03
camposyuri <yurinapoleao@gmail.com>
2021-02-25 20:53:04 -0300

    Criando function validadora

---
+++ backend/api/validation.js
@@ -0,0 +1,43 @@
+module.exports = (app) => {
+  // Existe ou erro -> Se existir passa pela função, senão ERROR!!!
+  // TEM QUE EXISTIR!!!!
+  function existsOrError(value, msg) {
+    if (!value) {
+      throw msg;
+    }
+
+    if (Array.isArray(value) && value.length === 0) {
+      throw msg;
+    }
+
+    if (typeof value === "string" && !value.trim()) {
+      throw msg;
+    }
+  }
+
+  // Se não existir passa, senão ERROR!!!
+  // TEM QUE NÃO EXISTIR!!!
+  function notExistsOrError(value, msg) {
+    try {
+      existsOrError(value, msg);
+    } catch (msg) {
+      // Se não existir ele passa tranquilo
+      return;
+    }
+    // Se existir ele lança o erro
+    throw msg;
+  }
+
+  // Dois valores se são iguais ou não
+  function equalsOrError(valueA, valueB, msg) {
+    if (valueA !== valueB) {
+      throw msg;
+    }
+  }
+
+  return {
+    existsOrError,
+    notExistsOrError,
+    equalsOrError,
+  };
+};
