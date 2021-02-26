2d4c4bc06e6fada33702ca5ca167e1d62f844905
camposyuri <yurinapoleao@gmail.com>
2021-02-25 21:02:17 -0300

    Criando function validadora

--- backend/api/validation.js
+++ backend/api/validation.js
@@ -4,14 +4,12 @@ module.exports = (app) => {
   function existsOrError(value, msg) {
     if (!value) {
       throw msg;
-    }
-
-    if (Array.isArray(value) && value.length === 0) {
+    } else if (Array.isArray(value) && value.length === 0) {
       throw msg;
-    }
-
-    if (typeof value === "string" && !value.trim()) {
+    } else if (typeof value === "string" && !value.trim()) {
       throw msg;
+    } else {
+      return;
     }
   }
 
