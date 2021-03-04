37f5f08c42f79aecb50089b12b00ec48390c2192
camposyuri <yurinapoleao@gmail.com>
2021-03-04 20:10:54 -0300

    Validando Usuário Administrador

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,3 +1,5 @@
+const admin = require("./admin");
+
 module.exports = (app) => {
   app.post("/signup", app.api.user.save);
   app.post("/signin", app.api.auth.signin);
@@ -6,16 +8,19 @@ module.exports = (app) => {
   app
     .route("/users")
     .all(app.config.passport.authenticate())
-    .get(app.api.user.get)
-    .post(app.api.user.save);
+    .get(admin(app.api.user.get))
+    .post(admin(app.api.user.save));
 
-  app.route("/users/:id").get(app.api.user.getById).put(app.api.user.save);
+  app
+    .route("/users/:id")
+    .get(admin(app.api.user.getById))
+    .put(admin(app.api.user.save));
 
   app
     .route("/categories")
     .all(app.config.passport.authenticate())
-    .get(app.api.category.get)
-    .post(app.api.category.save);
+    .get(admin(app.api.category.get))
+    .post(admin(app.api.category.save));
 
   // Cuidado com ordem! Tem que vir antes de /categories/:id
   app
@@ -27,21 +32,21 @@ module.exports = (app) => {
     .route("/categories/:id")
     .all(app.config.passport.authenticate())
     .get(app.api.category.getById)
-    .put(app.api.category.save)
-    .delete(app.api.category.remove);
+    .put(admin(app.api.category.save))
+    .delete(admin(app.api.category.remove));
 
   app
     .route("/articles")
     .all(app.config.passport.authenticate())
-    .get(app.api.articles.get)
-    .post(app.api.articles.save);
+    .get(admin(app.api.articles.get))
+    .post(admin(app.api.articles.save));
 
   app
     .route("/articles/:id")
     .all(app.config.passport.authenticate())
     .get(app.api.articles.getById)
-    .put(app.api.articles.save)
-    .delete(app.api.articles.remove);
+    .put(admin(app.api.articles.save))
+    .delete(admin(app.api.articles.remove));
 
   app
     .route("/categories/:id/articles")
